94b9c30a8259d1ff18ce5b23384ffb2f6fc9f096
Sevenings <lvlassis.2@gmail.com>
2024-09-14 22:09:01 -0300

    corrigido cubeEf

--- turtle/cubeEf.lua
+++ turtle/cubeEf.lua
@@ -1,10 +1,10 @@
 --Cube economic
-require "tuplus"
+tp = require "tuplus"
 
-function trialDig()
-    tryDig()
-    tryDigUp()
-    tryDigDown()
+local function trialDig()
+    tp.tryDig()
+    tp.tryDigUp()
+    tp.tryDigDown()
 end
 
 local dimension = vector.new(arg[1], arg[2], arg[3])
@@ -13,50 +13,50 @@ local quant_camadas = math.floor(dimension.y/3)
 local resto_camadas = dimension.y%3
 
 --Setup
-tryDigUp()
-up()
+tp.tryDigUp()
+tp.up()
 
 for y=1, quant_camadas do   --Cavar para cima
     for z=1, dimension.z do   --Cavar para a direita
         for x=1, dimension.x-1 do   --Cavar para frente
             trialDig()
-            forward()
+            tp.forward()
         end
 
         if z == dimension.z then
-            tryDigUp()
-            tryDigDown()
+            tp.tryDigUp()
+            tp.tryDigDown()
             break
         end
 
         if z%2 == 1 then    --Curva para direita
-            turnRight()
+            tp.turnRight()
             trialDig()
-            forward()
-            turnRight()
+            tp.forward()
+            tp.turnRight()
         else
-            turnLeft()
+            tp.turnLeft()
             trialDig()
-            forward()
-            turnLeft()
+            tp.forward()
+            tp.turnLeft()
         end
     end
 
     if dimension.z%2 == 1 then    --Ajeita a orientação
-        turnLeft()
-        turnLeft()
+        tp.turnLeft()
+        tp.turnLeft()
     else
-        turnRight()
+        tp.turnRight()
         for i=1, dimension.z-1 do
-            forward()
+            tp.forward()
         end
-        turnRight()
+        tp.turnRight()
     end 
     
     if y < quant_camadas then
         for i=1, 3 do
-            tryDigUp()
-            up()
+            tp.tryDigUp()
+            tp.up()
         end
     end
 end
@@ -66,15 +66,15 @@ if resto_camadas == 0 then
 end
 
 for i=1, 2 do
-    tryDigUp()
-    up()
+    tp.tryDigUp()
+    tp.up()
 end
 
 for z=1, dimension.z do   --Cavar para a direita
     for x=1, dimension.x-1 do   --Cavar para frente
-        if resto_camadas == 2 then tryDigUp() end
-        tryDig()
-        forward()
+        if resto_camadas == 2 then tp.tryDigUp() end
+        tp.tryDig()
+        tp.forward()
     end
 
     if z == dimension.z then
@@ -82,25 +82,25 @@ for z=1, dimension.z do   --Cavar para a direita
     end
 
     if z%2 == 1 then    --Curva para direita
-        turnRight()
-        tryDig()
-        forward()
-        turnRight()
+        tp.turnRight()
+        tp.tryDig()
+        tp.forward()
+        tp.turnRight()
     else
-        turnLeft()
-        tryDig()
-        forward()
-        turnLeft()
+        tp.turnLeft()
+        tp.tryDig()
+        tp.forward()
+        tp.turnLeft()
     end
 end
 
 if dimension.z%2 == 1 then    --Ajeita a orientação
-    turnLeft()
-    turnLeft()
+    tp.turnLeft()
+    tp.turnLeft()
 else
-    turnRight()
+    tp.turnRight()
     for i=1, dimension.z-1 do
-        forward()
+        tp.forward()
     end
-    turnRight()
+    tp.turnRight()
 end 
