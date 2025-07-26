44e57dd9e15eb88e7f6b9e0c498f506059eded56
Sevenings <lvlassis.2@gmail.com>
2025-07-25 23:14:17 -0300

    refact: tuplus

--- tuplus/tuplus.lua
+++ tuplus/tuplus.lua
@@ -3,6 +3,8 @@
     Turtle Plus eh um modulo para melhores funcoes de controle sobre as turtles.
 ]]
 
+local tuplus = {}
+
 
 -- Imports
 local ioutils = require "ioutils"
@@ -22,7 +24,7 @@ local MOVIMENTO = {
 
 
 -- Adiciona um movimento a todos os caminhos ate entao registrados
-local function registrarMovimento(movimento)
+function tuplus.registrarMovimento(movimento)
     for _, caminho in pairs(Caminhos) do
         if caminho.gravando then
             table.insert(caminho.movimentos, movimento)
@@ -40,7 +42,7 @@ WEST = 2
 NORTH = 3
 
 
-local function facingToString(direction)
+function tuplus.facingToString(direction)
     if direction == EAST then return "East"
     elseif direction == SOUTH then return "South"
     elseif direction == WEST then return "West"
@@ -51,7 +53,7 @@ end
 
 -- Status getters e setters
 local fileName = "status.config"
-local function getStatus()
+function tuplus.getStatus()
     if not fs.exists(fileName) then
         fs.open(fileName, "w")
     end
@@ -65,7 +67,7 @@ local function getStatus()
 end
 
 
-local function setStatus(newStatusTable)
+function tuplus.setStatus(newStatusTable)
     local file = io.open(fileName, "w")
     local content = textutils.serialize(newStatusTable)
     file:write(content)
@@ -75,7 +77,7 @@ end
 
 -- Facing getter e setter
 FACING = nil
-local function getFacing()
+function tuplus.getFacing()
     if FACING then
         return FACING
     end
@@ -85,12 +87,12 @@ local function getFacing()
 end
 
 
-local function setFacing(direction)
+function tuplus.setFacing(direction)
     FACING = direction
 end
 
 
-local function saveFacing()
+function tuplus.saveFacing()
     local status = getStatus()
     assert(status.facing)
     status.facing = FACING
@@ -100,7 +102,7 @@ end
 
 -- Position getter e setter
 POSITION = nil
-local function getPosition()
+function tuplus.getPosition()
     if POSITION then
         return POSITION
     end
@@ -111,12 +113,12 @@ local function getPosition()
 end
 
 
-local function setPosition(pos)
+function tuplus.setPosition(pos)
     POSITION = pos
 end
 
 
-local function savePosition()
+function tuplus.savePosition()
     local status = getStatus()
     assert(status.position)
     status.position = POSITION
@@ -125,7 +127,7 @@ end
 
 
 -- Conversor Facing(String) to Facing(Number)
-local function facingToNum(facing)
+function tuplus.facingToNum(facing)
     local facing = string.lower(facing)
     if facing == "w" then return WEST
     elseif facing == "s" then return SOUTH
@@ -136,7 +138,7 @@ local function facingToNum(facing)
 end
 
 
-local function facingToVector(facing)
+function tuplus.facingToVector(facing)
 	if facing == EAST then
 		return vector.new(1, 0, 0)
 	elseif facing == WEST then
@@ -149,7 +151,7 @@ local function facingToVector(facing)
 end
 
 
-local function vectorToFacing(vetor)
+function tuplus.vectorToFacing(vetor)
     if vetor:equals(vector.new(1, 0, 0)) then return EAST
     elseif vetor:equals(vector.new(-1, 0, 0)) then return WEST
     elseif vetor:equals(vector.new(0, 0, 1)) then return SOUTH
@@ -160,13 +162,13 @@ end
 
 --Orientation
 --Facing orientation
-local function askForDirection()
+function tuplus.askForDirection()
     local facing = ioutils.askFor(facingToNum, "Set the direction: [n, s, e, w]", "Not a valid direction...")
     return facing
 end
 
 
-local function gpsFindDirection()
+function tuplus.gpsFindDirection()
     local pos_a = gpsFindPosition()
     local tries = 0
     while turtle.detect() do
@@ -185,7 +187,7 @@ local function gpsFindDirection()
 end
 
 
-local function orientateFacing(option)
+function tuplus.orientateFacing(option)
     local modem = peripheral.find("modem")
     local facing_direction = 0
     if modem and gps.locate() and not (option == "manual") then
@@ -198,7 +200,7 @@ end
 
 
 --Position orientation
-local function askForPosition()
+function tuplus.askForPosition()
     local x = ioutils.askFor(tonumber, "Set X:", "Not a number...")
     local y = ioutils.askFor(tonumber, "Set Y:", "Not a number...")
     local z = ioutils.askFor(tonumber, "Set Z:", "Not a number...")
@@ -207,13 +209,13 @@ local function askForPosition()
 end
 
 
-local function gpsFindPosition()
+function tuplus.gpsFindPosition()
     local x, y, z = gps.locate()
     return vector.new(x, y, z)
 end
 
 
-local function orientatePosition(option)
+function tuplus.orientatePosition(option)
     local modem = peripheral.find("modem")
     local position = vector.new(0, 0, 0)
     if modem and gps.locate() and not (option == "manual") then
@@ -225,7 +227,7 @@ local function orientatePosition(option)
 end
 
 
-local function canGpsOrientate()
+function tuplus.canGpsOrientate()
     local modem = peripheral.find("modem")
     if modem and gps.locate() then
         return true
@@ -235,19 +237,19 @@ end
 
 
 --To orientate
-local function orientate(option)
+function tuplus.orientate(option)
     orientateFacing(option)
     orientatePosition(option)
 end
 
 
-local function facingDistance(dirInicial, dirFinal)    --Matemática circular :)
+function tuplus.facingDistance(dirInicial, dirFinal)    --Matemática circular :)
     return (dirFinal - dirInicial) % 4
 end
 
 
 -- Funções para rotacionar
-local function turnRight()
+function tuplus.turnRight()
     turtle.turnRight()
     local facing = getFacing()
     facing = facing + 1
@@ -257,7 +259,7 @@ local function turnRight()
 end
 
 
-local function turnLeft()
+function tuplus.turnLeft()
     turtle.turnLeft()
     local facing = getFacing()
     facing = facing - 1
@@ -267,7 +269,7 @@ local function turnLeft()
 end
 
 
-local function turnTo(targetDirection)
+function tuplus.turnTo(targetDirection)
     while getFacing() ~= targetDirection do
         if facingDistance(getFacing(), targetDirection) > 1 then
             turnLeft()
@@ -279,7 +281,7 @@ end
 
 
 --Digging Functions
-local function tryDig()
+function tuplus.tryDig()
     while turtle.detect() do
         turtle.dig()
         sleep(0.1)
@@ -287,7 +289,7 @@ local function tryDig()
 end
 
 
-local function tryDigUp()
+function tuplus.tryDigUp()
     while turtle.detectUp() do
         turtle.digUp()
         sleep(0.1)
@@ -295,7 +297,7 @@ local function tryDigUp()
 end
 
 
-local function tryDigDown()
+function tuplus.tryDigDown()
     while turtle.detectDown() do
         turtle.digDown()
         sleep(0.1)
@@ -304,7 +306,7 @@ end
 
 
 --Moving functions
-local function forward()
+function tuplus.forward()
     local moved = turtle.forward()
     if moved then
       local position = getPosition()
@@ -316,7 +318,7 @@ local function forward()
 end
 
 
-local function back()
+function tuplus.back()
     local moved = turtle.back()
     if moved then
       local position = getPosition()
@@ -328,7 +330,7 @@ local function back()
 end
 
 
-local function up()
+function tuplus.up()
   local moved = turtle.up()
   if moved then
     local position = getPosition()
@@ -339,7 +341,7 @@ local function up()
 end
 
 
-local function down()
+function tuplus.down()
   local moved = turtle.down()
 	if moved then
     local position = getPosition()
@@ -350,7 +352,7 @@ local function down()
 end
 
 
-local function walkTo(destination)
+function tuplus.walkTo(destination)
     local x = destination.x
     local y = destination.y
     local z = destination.z
@@ -389,27 +391,27 @@ local function walkTo(destination)
 end
 
 
-local function upUntil()
+function tuplus.upUntil()
     while up() do end
 end
 
 
-local function downUntil()
+function tuplus.downUntil()
     while down() do end
 end
 
 
-local function forwardUntil()
+function tuplus.forwardUntil()
     while forward() do end
 end
 
 
-local function backUntil()
+function tuplus.backUntil()
     while back() do end
 end
 
 
-local function smoothWalkTo(destination)
+function tuplus.smoothWalkTo(destination)
     while not getPosition().equals(destination) do
         local deslocamento = destination - getPosition()
         while deslocamento:dot(facingToVector(getFacing())) <= 0 do
@@ -447,27 +449,27 @@ local DICT_MOVIMENTO_R = {
 
 
 -- Retorna o caminho com certo nome
-local function getCaminho(nome_caminho)
+function tuplus.getCaminho(nome_caminho)
     return Caminhos[nome_caminho]
 end
 
 
 -- Retorna a lista de movimentos de um caminho
-local function getMovimentos(nome_caminho)
+function tuplus.getMovimentos(nome_caminho)
     local caminho = getCaminho(nome_caminho)
     return caminho.movimentos
 end
 
 
 -- Altera o valor de um caminho
-local function setCaminho(nome_caminho, caminho)
+function tuplus.setCaminho(nome_caminho, caminho)
     Caminhos[nome_caminho] = caminho
 end
 
 
 -- Cria um caminho novo na lista de caminhos
 -- Caminhos sao ativos por padrao
-local function novoCaminho(nome)
+function tuplus.novoCaminho(nome)
     local caminho = {
         -- Lista de movimentos do caminho
         movimentos = {},
@@ -483,19 +485,19 @@ local function novoCaminho(nome)
 end
 
 -- Remove um caminho da lista de caminhos
-local function deletarCaminho(nome_caminho)
+function tuplus.deletarCaminho(nome_caminho)
     Caminhos[nome_caminho] = nil
 end
 
 
 -- Verifica se um caminho existe
-local function existeCaminho(nome_caminho)
+function tuplus.existeCaminho(nome_caminho)
     return Caminhos[nome_caminho] ~= nil
 end
 
 
 -- Inicia a gravação de um caminho para poder ser atualizado
-local function iniciarGravacaoCaminho(nome_caminho)
+function tuplus.gravarCaminho(nome_caminho)
     if not existeCaminho(nome_caminho) then
         novoCaminho(nome_caminho)
     end
@@ -504,13 +506,16 @@ end
 
 
 -- Encerra a gravação de um caminho, para que nao possa ser alterado
-local function pararGravacaoCaminho(nome_caminho)
+function tuplus.pararGravacaoCaminho(nome_caminho)
+    if not existeCaminho(nome_caminho) then
+        return
+    end
     Caminhos[nome_caminho].gravando = false
 end
 
 
 -- Percorre um caminho
-local function percorrerCaminho(nome_caminho)
+function tuplus.percorrerCaminho(nome_caminho)
     local caminho = getCaminho(nome_caminho)
     for k, movimento in pairs(caminho.movimentos) do
         DICT_MOVIMENTO[movimento]()
@@ -519,7 +524,8 @@ end
 
 
 -- Percorre um caminho de tras para frente
-local function desfazerCaminho(nome_caminho)
+function tuplus.desfazerCaminho(nome_caminho)
+    pararGravacaoCaminho(nome_caminho)
     local caminho = getCaminho(nome_caminho)
     local movimentos = caminho.movimentos
     for i = #movimentos, 1, -1 do
@@ -530,12 +536,12 @@ end
 
 
 -- Limpa o registro de um caminho sem deleta-lo
-local function limparCaminho(nome_caminho)
+function tuplus.limparCaminho(nome_caminho)
     Caminhos[nome_caminho].movimentos = {}
 end
 
 -- Calcula o tamanho de um caminho
-local function tamanhoCaminho(nome_caminho)
+function tuplus.tamanhoCaminho(nome_caminho)
     local movimentos = getMovimentos(nome_caminho)
 
     local dict_gastos = {
@@ -557,7 +563,7 @@ end
 
 -- Salva os movimentos de um caminho em um nome_arquivo
 -- para que possam ser carregaos eventualmente
-local function salvarCaminho(nome_caminho, nome_arquivo)
+function tuplus.salvarCaminho(nome_caminho, nome_arquivo)
     local caminho = getCaminho(nome_caminho)
     local conteudo = textutils.serializeJSON(caminho)
     local file = io.open(nome_arquivo, "w")
@@ -570,7 +576,7 @@ local function salvarCaminho(nome_caminho, nome_arquivo)
     end
 end
 
-local function carregarCaminho(nome_caminho, nome_arquivo)
+function tuplus.carregarCaminho(nome_caminho, nome_arquivo)
     local file = io.open(nome_arquivo, "r")
     if not file then
         print("[Erro] Arquivo ".. nome_arquivo .."não existe")
@@ -584,7 +590,7 @@ end
 
 
 -- Funcoes de Inventario
-local function search(itemName)
+function tuplus.search(itemName)
     for slot=1, 16 do
         local item = turtle.getItemDetail(slot)
         if item then
@@ -598,7 +604,7 @@ local function search(itemName)
 end
 
 
-local function searchNotEmptySlot()
+function tuplus.searchNotEmptySlot()
     for slot=1, 16 do
         if turtle.getItemCount(slot) > 0 then 
             turtle.select(slot)
@@ -610,7 +616,7 @@ end
 
 
 -- Verifica se inventario esta cheio 
-local function inventarioCheio()
+function tuplus.inventarioCheio()
     local selected_slot = turtle.getSelectedSlot()
     for s=16, 1, -1 do
         turtle.select(s)
@@ -625,7 +631,7 @@ end
 
 
 -- Esvazia o inventario dropando todos os itens
-local function esvaziarInventario()
+function tuplus.esvaziarInventario()
     local selected_slot = turtle.getSelectedSlot()
     for s=1, 16 do
         turtle.select(s)
@@ -636,33 +642,33 @@ end
 
 
 -- Inspeciona para ver se o bloco da frente eh o desejado
-local function isBlock(block_name)
+function tuplus.isBlock(block_name)
     local _, block = turtle.inspect()
     return block.name == block_name
 end
 
 
 -- Inspeciona para ver se o bloco de cima eh o desejado
-local function isBlockUp(block_name)
+function tuplus.isBlockUp(block_name)
     local _, block = turtle.inspectUp()
     return block.name == block_name
 end
 
 
 -- Inspeciona para ver se o bloco de baixo eh o desejado
-local function isBlockDown(block_name)
+function tuplus.isBlockDown(block_name)
     local _, block = turtle.inspectDown()
     return block.name == block_name
 end
 
 
-local function distanceToMe(final_pos)
+function tuplus.distanceToMe(final_pos)
     local diff_vector = final_pos - (getPosition() + vector.new(0.5, 0.5, 0.5))
     return diff_vector:length()
 end
 
 
-local function closestPos(pos_list)
+function tuplus.closestPos(pos_list)
     local menor_distancia = distanceToMe(pos_list[1])
     local closest = pos_list[1]
     for i,posicao in ipairs(pos_list) do
@@ -677,7 +683,7 @@ end
 
 
 -- Funcoes de construcao
-local function place()
+function tuplus.place()
     canPlace, error = turtle.place()
     while not canPlace do
         if error == "Cannot place block here" then
@@ -692,7 +698,7 @@ local function place()
 end
 
 
-local function placeDown()
+function tuplus.placeDown()
     canPlace, error = turtle.placeDown()
     while not canPlace do
         if error == "Cannot place block here" then
@@ -707,7 +713,7 @@ local function placeDown()
 end
 
 
-local function placeUp()
+function tuplus.placeUp()
     canPlace, error = turtle.placeUp()
     while not canPlace do
         if error == "Cannot place block here" then 
@@ -723,87 +729,9 @@ end
 
 
 -- Funções de combustivel
-local function refuelAll()
+function tuplus.refuelAll()
     shell.run('refuel', 'all')
 end
 
 
-return {
-    facingToString = facingToString,
-    getFacing = getFacing,
-    setFacing = setFacing,
-    saveFacing = saveFacing,
-
-    getPosition = getPosition,
-    setPosition = setPosition,
-    savePosition = savePosition,
-
-    facingToNum = facingToNum,
-    facingToVector = facingToVector,
-    vectorToFacing = vectorToFacing,
-
-    askForDirection = askForDirection,
-    gpsFindDirection = gpsFindDirection,
-    orientateFacing = orientateFacing,
-
-    askForPosition = askForPosition,
-    gpsFindPosition = gpsFindPosition,
-    orientatePosition = orientatePosition,
-    orientate = orientate,
-    canGpsOrientate = canGpsOrientate,
-
-    facingDistance = facingDistance,
-    turnRight = turnRight,
-    turnLeft = turnLeft,
-    turnTo = turnTo,
-
-    tryDig = tryDig,
-    tryDigUp = tryDigUp,
-    tryDigDown = tryDigDown,
-
-    forward = forward,
-    back = back,
-    up = up,
-    down = down,
-    walkTo = walkTo,
-
-    upUntil = upUntil,
-    downUntil = downUntil,
-    forwardUntil = forwardUntil,
-    backUntil = backUntil,
-
-    smoothWalkTo = smoothWalkTo,
-
-    getCaminho = getCaminho,
-    setCaminho = setCaminho,
-    getMovimentos = getMovimentos,
-    comecarCaminho = novoCaminho,
-    deletarCaminho = deletarCaminho,
-    existeCaminho = existeCaminho,
-    iniciarGravacaoCaminho = iniciarGravacaoCaminho,
-    pararGravacaoCaminho = pararGravacaoCaminho,
-    registrarMovimento = registrarMovimento,
-    percorrerCaminho = percorrerCaminho,
-    desfazerCaminho = desfazerCaminho,
-    limparCaminho = limparCaminho,
-    tamanhoCaminho = tamanhoCaminho,
-    salvarCaminho = salvarCaminho,
-    carregarCaminho = carregarCaminho,
-
-    search = search,
-    searchNotEmptySlot = searchNotEmptySlot,
-    inventarioCheio = inventarioCheio,
-    esvaziarInventario = esvaziarInventario,
-    isBlock = isBlock,
-    isBlockUp = isBlockUp,
-    isBlockDown = isBlockDown,
-
-    distanceToMe = distanceToMe,
-    closestPos = closestPos,
-
-    place = place,
-    placeDown = placeDown,
-    placeUp = placeUp,
-
-    refuelAll = refuelAll,
-}
+return tuplus
