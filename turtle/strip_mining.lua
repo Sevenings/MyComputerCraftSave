98da3db9bc453f09ae1bc45f8c8a48778508820d
Sevenings <lvlassis.2@gmail.com>
2025-07-27 21:53:58 -0300

    adiciona submodulo artist

--- turtle/strip_mining.lua
+++ turtle/strip_mining.lua
@@ -1,14 +1,24 @@
 local tp = require 'tuplus'
 
-profundidade = 5
+local args = {...}
+local profundidade = tonumber(args[1]) or 64
 
 local function digNext()
     tp.tryDig()
+    tp.tryDigDown()
     tp.tryDigUp()
     tp.forward()
 end
 
 
-for i = 1, profundidade do
+-- Aguardando sinal
+while not redstone.getInput("left") do
+    sleep(0.1)
+end
+
+-- Main
+tp.tryDigUp()
+tp.up()
+for _ = 1, profundidade do
     digNext()
 end
